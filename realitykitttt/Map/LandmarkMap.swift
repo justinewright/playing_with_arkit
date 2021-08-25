a35433b0d91cfcf5e0a3f1fd108c5cd2e465cc30
justine wright <wright.justine21@gmail.com>
2021-08-25 11:17:50 +0200

    sets maxCapacity of area to 24%

--- realitykitttt/Map/LandmarkMap.swift
+++ realitykitttt/Map/LandmarkMap.swift
@@ -19,6 +19,7 @@ class LandmarkMap {
     private var centerY: Float
     private var xShift: Float
     private var yShift: Float
+    private var maxCapacity: Float = 0.24
 
     init(width: Float, height: Float, nodeDistance: Float, centerX: Float, centerY: Float) {
         self.centerX = centerX
@@ -35,11 +36,8 @@ class LandmarkMap {
         genMap(width: width, height: height)
     }
 
-    private func genMap(width: Float, height: Float) {
-        genMapUpperLeft(width: width, height: height)
-        genMapUpperRight(width: width, height: height)
-        genMapLowerLeft(width: width, height: height)
-        genMapLowerRight(width: width, height: height)
+    var spaceAvailable: Bool {
+        usedAreaPercentage < maxCapacity
     }
 
     func getRandomPosition() -> (Float, Float)? {
@@ -49,40 +47,50 @@ class LandmarkMap {
         }
         return nil
     }
+}
 
-    var spaceAvailable: Bool {
-        availablityMap.filter({ $0.value }).count == 0
+
+private extension LandmarkMap {
+    var usedAreaPercentage: Float {
+        Float(availablityMap.filter({ $0.value }).count) / Float(availablityMap.count)
     }
 
-    private func genMapUpperLeft(width: Float, height: Float) {
+    func genMap(width: Float, height: Float) {
+        genMapUpperLeft(width: width, height: height)
+        genMapUpperRight(width: width, height: height)
+        genMapLowerLeft(width: width, height: height)
+        genMapLowerRight(width: width, height: height)
+    }
+
+    func genMapUpperLeft(width: Float, height: Float) {
         let xMax = getNumberOfPoints(distance: width - xShift)
         let yMax = getNumberOfPoints(distance: height + yShift)
 
         addToMap(xMin: -xMax, xMax: 0, yMin: 0, yMax: yMax)
     }
 
-    private func genMapUpperRight(width: Float, height: Float) {
+    func genMapUpperRight(width: Float, height: Float) {
         let xMax = getNumberOfPoints(distance: width + xShift)
         let yMax = getNumberOfPoints(distance: height + yShift)
 
         addToMap(xMin: 0, xMax: xMax, yMin: 0, yMax: yMax)
     }
 
-    private func genMapLowerLeft(width: Float, height: Float) {
+    func genMapLowerLeft(width: Float, height: Float) {
         let xMax = getNumberOfPoints(distance: width - xShift)
         let yMax = getNumberOfPoints(distance: height - yShift)
 
         addToMap(xMin: -xMax, xMax: 0, yMin: -yMax, yMax: 0)
     }
 
-    private func genMapLowerRight(width: Float, height: Float) {
+    func genMapLowerRight(width: Float, height: Float) {
         let xMax = getNumberOfPoints(distance: width + xShift)
         let yMax = getNumberOfPoints(distance: height - yShift)
 
         addToMap(xMin: 0, xMax: xMax, yMin: -yMax, yMax: 0)
     }
 
-    private func addToMap (xMin: Int, xMax: Int, yMin: Int, yMax: Int) {
+    func addToMap (xMin: Int, xMax: Int, yMin: Int, yMax: Int) {
         for x in xMin...xMax {
             for y in yMin...yMax {
                 if availablityMap.keys.contains(CoordPairs(x: x, y: y)) {
